d571e103a636fded428de35acf8183bf88ba62f0
uKaiWen <94185976+uKaiWen@users.noreply.github.com>
2022-11-12 17:13:30 -0500

    Time: 100 ms (8.16%), Space: 14.2 MB (91.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,26 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        l, r = 0, len(nums) - 1
+        l,r = 0, len(nums) - 1
         
-        while l <= r:
-            mid = (l + r) // 2
+        while l <= r: 
+            mid = (l+r) // 2 #// if a list has 4 elements [0,1,2,3] 1 is mid
             
-            if nums[mid] == target:
+            if nums[mid] == target: 
                 return mid
             
-            # left sorted portion
+            # left partition
             elif nums[mid] >= nums[l]:
                 if nums[l] <= target <= nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
-                    
-            # right sorted portion
+            
+            # right partition
             else:
                 if nums[mid] <= target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
-                    
+        
+        # target doesn't exist
         return -1
+                
