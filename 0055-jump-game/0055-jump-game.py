c55fd706fba6e97063baf62bfe8930e02b5006bb
Kelvin Yu <94185976+kelvin-u@users.noreply.github.com>
2022-11-16 18:14:56 -0500

    Time: 1120 ms (57.85%), Space: 15.2 MB (82.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,12 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums) - 1
-        # start at the last index, keep moving right to left
-        for i in range(len(nums) - 2, -1, -1): #goal is already the last element
-            if i + nums[i] >= goal: #nums[i] is the jump length the num itself in nums
-                goal = i
+        # the -1 in second range to 0, -1 step backwards
+        for i in range(len(nums)-2, -1, -1): # goal is already at last posistion
+            if i + nums[i] >= goal:
+                goal = i # shift the goal to i
         
-        # return True if goal == 0 else False
         return goal == 0
-    
     
