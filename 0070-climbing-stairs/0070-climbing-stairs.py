2e9708ece21d4b9d68a695a06253f052b25d8e5c
Kelvin Yu <94185976+kelvin-u@users.noreply.github.com>
2022-11-18 18:47:02 -0500

    Time: 45 ms (73.53%), Space: 13.7 MB (99.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1,1
+        one, two = 1,1 
         
         for i in range(n-1):
-            temp = one #
-            one = one + two # the new one is basically the old (one + two)
-            two = temp # two is now shifted to the one position
+            temp = one
+            one = one + two 
+            two = temp
         return one
-            
             
