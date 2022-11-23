e5a4225f01ae04fadb23c6118277e9774d9f3d2a
Kelvin Yu <94185976+kelvin-u@users.noreply.github.com>
2022-11-23 16:54:51 -0500

    Time: 415 ms (31.47%), Space: 14.7 MB (83.21%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": return "" # if t is empty returns empty string
+        
+        countT, window = {}, {} # empty hashmaps
+        
+        # populate countT
+        for c in t:
+            countT[c] = 1 + countT.get(c,0)
+        
+        have, need = 0, len(countT)
+        result, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c,0)
+            
+            if c in countT and window[c] == countT[c]:
+                have += 1
+                
+            while have == need:
+                # update our result
+                # size of window
+                if (r - l + 1) < resLen:
+                    result = [l,r]
+                    resLen = (r - l + 1)
+                # pop from the left of our window
+                window[s[l]] -= 1
+                
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        
+        l, r = result 
+        return s[l:r+1] if resLen != float("infinity") else ""
+        
+        
