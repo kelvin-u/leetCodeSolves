120a6a0241e6d69c9b28ce236fdfa238916e79af
Kelvin Yu <94185976+kelvin-u@users.noreply.github.com>
2022-11-30 18:11:26 -0500

    Time: 4185 ms (67.56%), Space: 14 MB (53.13%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         # initialize rows and cols
-            ROWS, COLS = len(board), len(board[0])
+            rows, cols = len(board), len(board[0])
             
             def dfs(r,c,i):
-            
                 if i == len(word):
                     return True
 
-                if (r <0  or c <0 or r >= ROWS or c >= COLS or word[i]!= board[r][c]):
+                if (r < 0  or c < 0 or 
+                    r >= rows or c >= cols or 
+                    word[i]!= board[r][c]):
                     return False
-
 
                 temp = board[r][c]
                 board[r][c] = "#"
 
-                res = (dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1))
+                res = (dfs(r+1, c, i+1) or 
+                       dfs(r-1, c, i+1) or 
+                       dfs(r, c+1, i+1) or 
+                       dfs(r, c-1, i+1))
 
                 board[r][c] = temp 
 
                 return res
 
-            for r in range(ROWS):
-                for c in range(COLS):
+            for r in range(rows):
+                for c in range(cols):
                     if board[r][c] == word[0]:
                         if dfs(r,c,0):
                             return True
-
+                        
             return False
     
-    # O(n*m*4^n)
+    # O(n*m*4^n) TC
